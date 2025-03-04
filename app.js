ae7806794177ffd2df55b7a8817f73f9b2ae85d9
Marcos-Sobral <vini15oficial@gmail.com>
2025-03-04 13:37:14 -0300

    ✨ feat: verificar Chute( )

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 let numeroSecreto = gerarNumeroAleatorio();
-console.log(numero);
+//console.log(numeroSecreto);
+
+function verificarChute() {
+    let chute = document.querySelector("input").value;
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela("h1","Acertou!");
+        exibirTextoNaTela("p","Você descobriu o número secreto!");
+    } else{
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela("p","O número secreto é menor");  
+        }else{
+            exibirTextoNaTela("p", "O número secreto é maior");
+        }
+    }
+}
+
+function verificarNumero(numero) {
+    if (numero > 0) {
+        console.log("O número é positivo.");
+    }else if(numero < 0){
+        console.log("O número é negativo.");
+    }else{
+        console.log("O número é zero.");
+    }
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
 function exibirTextoNaTela(tag, texto) {
     let campo = window.document.querySelector(tag);
     campo.innerHTML = texto;
 };
 
+
 exibirTextoNaTela("h1","Jogo do numero secreto");
 exibirTextoNaTela("p","Escolha um numero entre 1 a 10");
-
-document.getElementsByClassName('container_botao');
-function verificarChute() {
-    let chute = document.querySelector("input").value;
-    console.log(chute == numeroSecreto);
-}
-
-function reiniciarJogo() {  }
-
-function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
-}
---
+++ desafio2/app.js
@@ -0,0 +1,13 @@
+function exibirMensagemNoConsole() {
+    console.log("Olá, Mundo!");
+}
+
+function exibirNome(nome) {
+    console.log(`Olá, ${nome}`);
+}
+
+function dobroValor(numero) {
+    return numero * 2;
+}
+
+
