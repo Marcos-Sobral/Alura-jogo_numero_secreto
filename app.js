2a33f065e468891c668ef8688afcfbee4e2b8c8d
Marcos-Sobral <vini15oficial@gmail.com>
2025-03-04 16:12:55 -0300

    ✨ feat: Adicionando web Speech

--- app.js
+++ app.js
@@ -10,6 +10,22 @@ function exibirTextoNaTela(tag, texto) {
     responsiveVoice.speak(texto,'Brazilian Portuguese Female', {rate: 1.2});
 }
 
+/*Opção sem usar o responsive Voice
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
+}
+
+*/
+
 function exibirMensagemInicial() {
     exibirTextoNaTela("h1","Jogo do numero secreto");
     exibirTextoNaTela("p","Escolha um numero entre 1 a 10");
