b0c1c544bf3faf50d43f575c8cf9fcef2dc23519
Marcos-Sobral <vini15oficial@gmail.com>
2025-03-04 13:56:56 -0300

    ✨ feat: contagem de tentativas

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 //console.log(numeroSecreto);
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = window.document.querySelector(tag);
+    campo.innerHTML = texto;
+};
+
+
+exibirTextoNaTela("h1","Jogo do numero secreto");
+exibirTextoNaTela("p","Escolha um numero entre 1 a 10");
 
 function verificarChute() {
     let chute = document.querySelector("input").value;
     if (chute == numeroSecreto) {
         exibirTextoNaTela("h1","Acertou!");
-        exibirTextoNaTela("p","Você descobriu o número secreto!");
+        let palavraTentativa = tentativas > 1 ? "Tentativas!":"Tentativa!";
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela("p",mensagemTentativas);
     } else{
         if (chute > numeroSecreto) {
             exibirTextoNaTela("p","O número secreto é menor");  
         }else{
             exibirTextoNaTela("p", "O número secreto é maior");
         }
+        tentativas = tentativas + 1;
     }
 }
 
@@ -28,12 +41,3 @@ function verificarNumero(numero) {
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
 }
-
-function exibirTextoNaTela(tag, texto) {
-    let campo = window.document.querySelector(tag);
-    campo.innerHTML = texto;
-};
-
-
-exibirTextoNaTela("h1","Jogo do numero secreto");
-exibirTextoNaTela("p","Escolha um numero entre 1 a 10");
